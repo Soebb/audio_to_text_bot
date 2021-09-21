ea6d9faaf3c9933cc93435252d6dfe38dbb0395b
Ed Carlos Bicudo <ed-carlo@hotmail.com>
2021-09-21 15:19:40 -0300

    Convertendo audio em texto

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import tokens
 import telebot
 import requests
+import speech_recognition as sr
+from pydub import AudioSegment
+import os
 import pretty_errors
 
 bot = telebot.AsyncTeleBot(tokens.BOT_TOKEN)
+r = sr.Recognizer()
+pasta_de_audio = os.path.join(os.getcwd(), 'audios')
 
 @bot.message_handler(commands=["start", "help"])
 def send_welcome(message):
@@ -19,14 +24,89 @@ def send_welcome(message):
 
     bot.reply_to(message, text).wait()
 
+
 @bot.message_handler(content_types=['voice'])
 def recebe_audio(message):
+    """Recebe um 'voice' do Telegram e retorna a mensagem em texto.
+
+    Args:
+        message (Message): Mensagem recebida do usuário.
+    """
 
     file_info = bot.get_file(message.voice.file_id).wait()
 
+    file_name = file_info.file_path.split('/')[1]
+
     file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(tokens.BOT_TOKEN, file_info.file_path))
 
-    bot.reply_to(message, "Ainda em desenvolvimento").wait()
+    audio = converter_audio(file_name, file)
+
+    response = extrair_texto(audio)
+
+    esvaziar_pasta()
+
+    bot.reply_to(message, response).wait()
+
+
+def converter_audio(file_name, file):
+    """Converte o audio recebido do Telegram em formato ogg para wav.
+
+    Args:
+        file_name (String): Nome do arquivo de audio
+        file (Raw): Conteúdo do arquivo
+
+    Returns:
+        Path: Caminho do arquivo convertido.
+    """
+    if not os.path.exists(pasta_de_audio):
+        os.makedirs(pasta_de_audio)
+
+    path_ogg = os.path.join(pasta_de_audio, f"{file_name}.ogg")
+    path_wav = os.path.join(pasta_de_audio, f"{file_name}.wav")
+
+    with open(path_ogg, 'wb') as f:
+        f.write(file.content)
+
+    sound = AudioSegment.from_ogg(path_ogg)
+    sound.export(path_wav, format="wav")
+
+    return path_wav
+
+
+def esvaziar_pasta():
+    """Excluí todos os arquivos da pasta 'audio'
+    """
+
+    for filename in os.listdir(pasta_de_audio):
+        file_path = os.path.join(pasta_de_audio, filename)
+
+        try:
+            os.remove(file_path)
+        except Exception as e:
+            print(f"Failed to delete {file_path}. Reason: {e}")
+
+
+def extrair_texto(audio):
+    """Envia o audio para a Engine do Google converter em texto.
+
+    Args:
+        audio (Path): Caminho do arquivo de audio
+
+    Returns:
+        String: Texto extraído do audio
+    """
+
+    with sr.AudioFile(audio) as source:
+
+        audio_data = r.record(source)
+
+        try:
+            text = r.recognize_google(audio_data,language='pt-BR')
+        except sr.UnknownValueError:
+            text = "Texto não entendido"
+
+        return text
+
 
 if __name__ == "__main__":
 
